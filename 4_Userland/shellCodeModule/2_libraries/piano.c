567a6db1bf73b3f9a3e7d97212e13cd67d1d47da
fedekloberdanz <fedeklober@gmail.com>
2025-06-09 22:58:12 +0000

    comentarios en todos los .c para usar la licencia gratis de pvs-studio

--- 4_Userland/shellCodeModule/2_libraries/piano.c
+++ 4_Userland/shellCodeModule/2_libraries/piano.c
@@ -1,3 +1,5 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 #include <piano.h>
 
 //DO:W, RE:E, MI:R, FA:T, SOL:Y, LA:U, SI:I
